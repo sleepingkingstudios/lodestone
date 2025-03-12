4e77f842416b68bbe5e8fff15ef9c8104829be76
Rob Smith <sleepingkingstudios@gmail.com>
2025-03-12 18:52:31 -0400

    Add Librum::Core, Librum::Iam dependencies.

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module Lodestone
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.autoload_lib(ignore: %w[tasks])
+    config.autoload_paths << "#{Librum::Core::Engine.root}/lib"
+    config.autoload_paths << "#{Librum::Iam::Engine.root}/lib"
 
     # Don't generate system test files.
     config.generators.system_tests = nil
