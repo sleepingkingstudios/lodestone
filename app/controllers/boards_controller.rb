5614a5a77ecb9ff76195de07451afe8e7be1a734
Rob Smith <sleepingkingstudios@gmail.com>
2025-03-17 01:51:28 -0400

    Clean up BoardsController.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,28 +7,11 @@ class BoardsController < Librum::Core::ViewController
       Librum::Core::Resources::BaseResource.new(name: 'board', singular: true)
   end
 
+  layout 'application'
+
   responder :html, Cuprum::Rails::Responders::Html::Resource
 
   action :show,
     Cuprum::Rails::Action.subclass(command_class: Boards::Commands::Show),
     member: false
-
-  private
-
-  def grouped_tasks
-    tasks = Task.order(updated_at: :desc)
-    tasks = tasks.where(project: project) if project
-    tasks.group_by(&:status)
-  end
-
-  def project
-    return @project if @project
-    return nil      if project_id.blank?
-
-    @project = Project.find(project_id)
-  end
-
-  def project_id
-    params[:project_id]
-  end
 end
