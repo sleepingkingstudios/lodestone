f568bed5cc96ed357a6a74af4ba8c383eaad0264
Rob Smith <sleepingkingstudios@gmail.com>
2025-05-11 00:32:41 -0400

    Remove deprecated files.

--- config/initializers/zeitwerk.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'actions'
-require 'models'
-
-loader = Rails.autoloaders.main
-
-loader.push_dir(Rails.root.join('lib/actions'), namespace: Actions)
-loader.push_dir(Rails.root.join('lib/models'), namespace: Models)
