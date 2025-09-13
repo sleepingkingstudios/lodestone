52861b16b489f3822c9bc64e691f61b8d02a9bf6
Rob Smith <sleepingkingstudios@gmail.com>
2025-09-12 23:08:37 -0400

    Add ENV flag to enable page layout for Projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,8 @@ require 'librum/core/commands/resources/new'
 # Controller for managing Project entities.
 class ProjectsController < BaseController
   def self.resource # rubocop:disable Metrics/MethodLength
-    Cuprum::Rails::Resource.new(
+    Librum::Components::Resource.new(
+      components:           Lodestone::Projects::View::Components,
       default_order:        :name,
       entity_class:         ::Project,
       permitted_attributes: %w[
@@ -19,6 +20,14 @@ class ProjectsController < BaseController
       ]
     )
   end
+
+  # :nocov:
+  if ENV['PAGE_LAYOUT'] == 'true'
+    layout 'page'
+
+    responder :html, Librum::Core::Responders::Html::ResourceResponder
+  end
+  # :nocov:
 
   action :create,
     Cuprum::Rails::Actions::Resources::Create,
