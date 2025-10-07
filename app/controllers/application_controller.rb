0e31624b8a9dd71dbd6f342f5f7469a9a5ca074b
Rob Smith <sleepingkingstudios@gmail.com>
2025-10-07 11:50:50 -0400

    [skip-ci] Add Librum::Iam authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,5 @@
 
 # Abstract base controller.
 class ApplicationController < ActionController::Base
-  class << self
-    private
-
-    def password
-      ENV.fetch('BASIC_AUTH_PASSWORD') do
-        Rails.application.credentials.authentication[:password]
-      end
-    end
-
-    def username
-      ENV.fetch('BASIC_AUTH_USERNAME') do
-        Rails.application.credentials.authentication[:username]
-      end
-    end
-  end
-
-  http_basic_authenticate_with name: username, password: password
-
-  layout 'legacy'
+  layout 'page'
 end
